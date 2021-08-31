98e902cb58edcd5f86bb16dfb28f65cd88474d76
guodong.An <2890815038@qq.com>
2021-08-31 10:02:37 +0800

    最新

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
 # super-compress
 
-## 基于 webpack 的一款图片压缩插件
-
-### webpack.config.js
+## 基于 webpack plugin 的一款图片压缩插件
 
 ```bash
 npm install super-compress
 ```
 
+### 参数介绍
+
+| 参数      |    类型 | 默认值 | 描述                             |
+| --------- | ------: | :----: | -------------------------------- |
+| min       |  number |   ""   | 最小值                           |
+| max       |  number |   ""   | 最大值                           |
+| key       |  string |   ""   | 密钥                             |
+| immediate | boolean | false  | 启动时是否压缩当前目录未压缩图片 |
+
+### 应用案例
+
+#### webpack.config.js
+
 ```js
-new ImageCompress({
-  dir: path.resolve("src/assets"), // 需要监听的文件目录
-  min: 1024 * 50, // 最小size不作处理
-  max: 1024 * 1024 * 2, // 最大size不作处理
-  key: "CX6j0LbSlRKt1X31DR44tNT67TmyDKCf", // tinypng秘钥 https://tinypng.com/
-});
+const ImageCompress = require("super-compress");
+module.exports = {
+  plugins: [
+    new ImageCompress({
+      min: 1024 * 50, // 最小阈值
+      max: 1024 * 1024 * 20, // 最大阈值
+      key: "CX6j0LbSlRKt1X31DR44tNT67TmyDKCf", // tinypng 密钥 tinypng.com获取
+      immediate: true, // 初始化时是否需要压缩已存在目录里的图片
+    }),
+  ],
+};
 ```
--- src/main.js
+++ src/main.js
@@ -1,49 +1,86 @@
 const watch = require('node-watch');
-const path = require("path")
-const tinify = require("tinify");
 const fs = require('fs');
-tinify.key = "CX6j0LbSlRKt1X31DR44tNT67TmyDKCf";
-const SOURCE_COMPRESS_STORE = "source-compress-store.json"
-const CONTENT_TYPE = "utf-8"
-var temp = null
+const join = require('path').join;
+const tinify = require("tinify");
+
+const regx = new RegExp(/(\.jpg|\.png|\.jpeg)$/)
+// tinify.key = "CX6j0LbSlRKt1X31DR44tNT67TmyDKCf";
+
+
+
 class ImageCompress {
-    dir = ""
-    min = 1024 * 50
-    max = 1024 * 1024 * 20
+    getJsonFiles(jsonPath) {
+        let jsonFiles = [];
+        function findJsonFile(path) {
+            let files = fs.readdirSync(path);
+            files.forEach(function (item, index) {
+                let fPath = join(path, item);
+                let stat = fs.statSync(fPath);
+                if (stat.isDirectory() === true) {
+                    findJsonFile(fPath);
+                }
+                if (stat.isFile() === true) {
+                    jsonFiles.push(fPath);
+                }
+            });
+        }
+        findJsonFile(jsonPath);
+        return jsonFiles;
+    }
     apply(compiler) {
-        compiler.hooks.compilation.tap("ImageCompress", () => {
-            try {
-                temp = new Map(Object.entries(JSON.parse(fs.readFileSync(SOURCE_COMPRESS_STORE, CONTENT_TYPE))));
-            } catch (err) {
-                temp = new Map()
-                fs.writeFileSync(SOURCE_COMPRESS_STORE, JSON.stringify(Object.create(null)))
+
+        compiler.hooks.emit.tap("assets", () => {
+            if (this.immediate) {
+                const res = this.getJsonFiles(this.dir)
+                const len = res.length;
+                let index = 0;
+                while (len > index) {
+                    const f = res[index];
+                    if (regx.test(f)) {
+                        this.compress("update", f)
+                    }
+                    index++;
+                }
+                this.immediate = false
             }
+        })
+        compiler.hooks.compilation.tap("ImageCompress", () => {
             watch(this.dir, {
                 recursive: true, filter(f) {
-                    return /(\.jpg|\.png|\.jpeg)$/.test(f);
+                    return regx.test(f);
                 }
-            }, async function (evt, file) {
-                let key = file?.replace(path.resolve(), "")
-                if (key && (evt !== "update" || temp.has(key))) return;
-                let stat = fs.statSync(file)
-                if (stat.size < this.min || stat.size > this.max) return;
-                temp.set(key, false)
-                tinify.fromFile(file).toFile(file).then(() => {
-                    temp.set(key, true)
-                    console.log(`${file}已自动压缩处理`)
-                    const data = JSON.stringify(Object.fromEntries(temp));
-                    fs.writeFileSync(SOURCE_COMPRESS_STORE, data);
-                })
-
-            })
+            }, this.compress)
 
         })
     }
 
-    constructor({ dir, min, max, key }) {
-        this.dir = dir
-        this.min = min
-        this.max = max
+
+
+
+    compress(evt, file) {
+        let fileMap = `${file}.map`
+        if (evt === "remove" && fs.existsSync(fileMap)) {
+            fs.unlink(fileMap, () => {
+                console.log(`${fileMap}已被删除`)
+            })
+            return;
+        }
+
+        if (evt !== "update" || fs.existsSync(fileMap)) return;
+        let stat = fs.statSync(file)
+        if (stat.size < this.min || stat.size > this.max) return;
+        tinify.fromFile(file).toFile(file).then(() => {
+            fs.writeFileSync(fileMap, `${file} is compressed`)
+        }).catch(err => {
+            fs.writeFileSync(fileMap, `${file} is ${err}`)
+        })
+        return true
+    }
+    constructor({ dir, min, max, key, immediate }) {
+        this.dir = dir // 需要观察的目录
+        this.min = min // 小于min则不会处理压缩
+        this.max = max  // 大于max则不会处理压缩
+        this.immediate = immediate // 初始化时是否需要压缩已存在目录里的图片
         tinify.key = key;
     }
 }
